a4ddcd2feec0e54256d75ccc476c2dd81b7c257f
andresg6 <ajcapanzana@gmail.com>
2018-09-06 18:27:57 -0700

    inclass

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,57 @@
 
-
-var correct= value="yes";
-var incorrect= value="no";
-var unanswered= value=null;
-
-
-
-
-
 $(document).ready(function() {
 
- $("#btn").on("click", function() {
+    var correct= value="yes";
+    var incorrect= value="no";
+    var unanswered= value=null;
 
- })
+    //  Interval Demonstration
+    //  Set our number counter to 30 seconds.
+    var number = 30;
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+    //  When the resume button gets clicked, execute the run function.
+    $("#button").on("click", run);
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    //  *****BUG FIX******** 
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+    //  The decrement function.
+    function decrement() {
+      //  Decrease number by one.
+      number--;
+      //  Show the number in the #show-number tag.
+      $("#show-number").html("<h2>" + number + "</h2>");
+      //  Once number hits zero...
+      if (number === 0) {
+        //  ...run the stop function.
+        stop();
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+    //  The stop function
+    function stop() {
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+      $("#show-results").html(
+          "<h2>" + "Correct answers:" + correct + "</h2>",
+          "<h2>" + "Incorrect answers:" + incorrect + "</h2>",
+          "<h2>" + "Unanswered:" + unanswered + "</h2>",
+          
+    );
 
- //Below is an example how a clicking something can run an additional function. 
- $(".invis-button").on("click", function() {
-    $(".captain-planet").animate({
-      opacity: "0.05"
-    });
-  });
+    }
+    //  Execute the run function.
+    run();
+
 
 })
 
